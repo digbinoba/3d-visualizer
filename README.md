115a9f5b809f4cbe6b377657b1e2ef3f1ce90591
Delano Igbinoba <delanoigbinoba@gmail.com>
2023-11-01 15:26:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Music Visualizer Tutorial
+
+## https://youtu.be/QRpCmBZpBU0
+[![Three js Music Visualizer With Shaders For Beginners](https://user-images.githubusercontent.com/64514807/227888729-26b2ebd0-56d9-409f-b2e6-12e7109f6f84.png)](https://youtu.be/QRpCmBZpBU0)
+
+
+This is the starter repo and it's based on my Three Webpack boilerplate. 
+Three.js webpack 5 using Babel, PostCSS, Sass and Gsap.
+
+## Installation
+
+Clone this repo and npm install.
+
+```bash
+npm i
+```
+
+## Usage
+
+### Development server
+
+```bash
+npm start
+```
+
+You can view the development server at `localhost:8080`.
+
+### Production build
+
+```bash
+npm run build
+```
+
+> Note: Install [http-server](https://www.npmjs.com/package/http-server) globally to deploy a simple server.
+
+```bash
+npm i -g http-server
+```
+
+You can view the deploy by creating a server in `dist`.
+
+```bash
+cd dist && http-server
+```
+
+## Features
+
+- [Three](https://threejs.org)
+- [Webpack](https://webpack.js.org/)
+- [Babel](https://babeljs.io/)
+- [Sass](https://sass-lang.com/)
+- [PostCSS](https://postcss.org/)
+- [Gsap](https://greensock.com/gsap/)
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
---
+++ src/app.js
@@ -0,0 +1,154 @@
+import * as THREE from "three";
+import {
+  addPass,
+  useCamera,
+  useGui,
+  useRenderSize,
+  useScene,
+  useTick,
+} from "./render/init.js";
+// import postprocessing passes
+import { SavePass } from "three/examples/jsm/postprocessing/SavePass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { BlendShader } from "three/examples/jsm/shaders/BlendShader.js";
+import { CopyShader } from "three/examples/jsm/shaders/CopyShader.js";
+import gsap from "gsap";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+
+import { SoftGlitchPass } from "./shaders/passes/SoftGlitch.js";
+import TRACK1 from "./sounds/fire.mp3";
+class Visualizer {
+  constructor(mesh, frequencyUniformName) {
+    //mesh setup
+    this.mesh = mesh;
+    this.frequencyUniformName = frequencyUniformName;
+    this.mesh.material.uniforms[this.frequencyUniformName] = { value: 0 };
+
+    //audio listener
+    this.listener = new THREE.AudioListener();
+    this.mesh.add(this.listener);
+
+    //global audio source
+
+    this.sound = new THREE.Audio(this.listener);
+    this.loader = new THREE.AudioLoader();
+
+    // analyzer
+    this.analyzer = new THREE.AudioAnalyser(this.sound, 32);
+  }
+
+  load(path) {
+    this.loader.load(path, (buffer) => {
+      this.sound.setBuffer(buffer);
+      this.sound.setLoop(true);
+      this.sound.setVolume(0.5);
+      this.sound.play();
+    });
+  }
+
+  getFrequency() {
+    return this.analyzer.getAverageFrequency();
+  }
+
+  update() {
+    const freq = Math.max(this.getFrequency() - 80, 0) / 50;
+
+    const freqUniform = this.mesh.material.uniforms[this.frequencyUniformName];
+    gsap.to(freqUniform, {
+      duration: 1.5,
+      ease: "Slow.eastOut",
+      value: freq,
+    });
+    return freq;
+  }
+}
+const startApp = () => {
+  const scene = useScene();
+  const camera = useCamera();
+  const gui = useGui();
+  const { width, height } = useRenderSize();
+
+  const ROTATION_SPEED = 0.02;
+  const MOTION_BLUR_AMOUNT = 0.725;
+
+  const dirLight = new THREE.DirectionalLight("#ffffff", 1);
+  const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+  scene.add(dirLight, ambientLight);
+
+  const geometry = new THREE.SphereGeometry(1, 100, 100);
+  const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {
+      uTime: {
+        value: 0,
+      },
+    },
+  });
+  const ico = new THREE.Mesh(geometry, material);
+  const wireframe = new THREE.LineSegments(geometry, material);
+  const WIREFRAME_DELTA = 0.015;
+  wireframe.scale.setScalar(1 + WIREFRAME_DELTA);
+  ico.add(wireframe);
+
+  const visualizer = new Visualizer(ico, "uAudioFrequency");
+  //visualizer.load(TRACK1)
+  scene.add(ico);
+
+  // GUI
+  const cameraFolder = gui.addFolder("Camera");
+  cameraFolder.add(camera.position, "z", 0, 10);
+  cameraFolder.open();
+
+  const audioFolder = gui.addFolder("Music");
+  const myObject = {
+    startMusic: function () {
+      visualizer.load(TRACK1);
+    },
+  };
+  audioFolder.add(myObject, "startMusic");
+  // postprocessing
+  const renderTargetParameters = {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    stencilBuffer: false,
+  };
+
+  // save pass
+  const savePass = new SavePass(
+    new THREE.WebGLRenderTarget(width, height, renderTargetParameters)
+  );
+
+  // blend pass
+  const blendPass = new ShaderPass(BlendShader, "tDiffuse1");
+  blendPass.uniforms["tDiffuse2"].value = savePass.renderTarget.texture;
+  blendPass.uniforms["mixRatio"].value = MOTION_BLUR_AMOUNT;
+
+  // output pass
+  const outputPass = new ShaderPass(CopyShader);
+  outputPass.renderToScreen = true;
+
+  // adding passes to composer POST PROCESSING
+  addPass(blendPass);
+  addPass(savePass);
+  addPass(outputPass);
+
+  const softGlitch = new SoftGlitchPass();
+  softGlitch.factor = 1;
+  addPass(softGlitch);
+  const animateIco = () => {
+    ico.rotation.x += ROTATION_SPEED;
+    ico.rotation.y += ROTATION_SPEED;
+  };
+
+  useTick(({ timestamp, timeDiff }) => {
+    //animateIco()
+    material.uniforms.uTime.value = timestamp / 1000;
+    const freq = visualizer.update();
+
+    softGlitch.factor = freq > 0.6 ? 0.7 : 0.1;
+  });
+};
+
+export default startApp;
